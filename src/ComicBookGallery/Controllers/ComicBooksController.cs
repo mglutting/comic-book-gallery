5cf49967ae4ff7be35f4d19ac4d8792e75c1ec05
Matt Glutting <mglutting@bluperspective.com>
2017-05-08 15:55:17 -0400

    added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +12,8 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-   
-                     new Artist() { Name = "Dan Slott", Role ="Script"},
-                     new Artist() { Name = "Humberto Ramos", Role ="Pencils"},
-                     new Artist() { Name = "Victor Olazaba", Role ="Inks"},
-                     new Artist() { Name = "Edgar Delgado", Role ="Colors"},
-                     new Artist() { Name = "Chris Eliopoulos", Role ="Letters"}    
-                }
-            };
-
-            return View(comicBook);
+            ComicBookRepository comics = new ComicBookRepository();
+            return View(comics.GetComicBook(700));
         }
     }
 }
